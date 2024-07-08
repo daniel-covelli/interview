2f8eca84bef6136f080cc382e9c855945f4b9f96
Daniel Covelli <57047397+daniel-covelli@users.noreply.github.com>
2024-07-08 14:44:16 -0700

    add websocket project and reorganize fetch project

--- react-interview-questions/fetch/src/App.tsx
+++ react-interview-questions/fetch/src/App.tsx
@@ -3,22 +3,20 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [results, setResults] = useState<any[]>([]);
   useEffect(() => {
-    (async () => {
-      const result = await (
-        await fetch(
-          "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=2"
-        )
-      ).json();
-      console.log("res", JSON.stringify(result, null, 2));
+    fetch("https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=2")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('data', JSON.stringify(data, null, 2));
 
-      setResults(result);
-    })();
+      setResults(data)
+    })
+
   }, []);
 
   return (
     <div style={{ flexDirection: "column" }}>
       {results.map((res) => (
-        <div id={res._id}>{res.type}</div>
+        <div key={res._id}>{res.type}</div>
       ))}
     </div>
   );
