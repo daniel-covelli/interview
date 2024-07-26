cc18afa7f354837c227436e031530404fc54c451
Daniel Covelli <57047397+daniel-covelli@users.noreply.github.com>
2024-07-25 20:53:36 -0700

    add bstToArray method and clean stuff up

--- typescript-interview-questions/src/class/bst.ts
+++ typescript-interview-questions/src/class/bst.ts
@@ -1,40 +1,100 @@
-import { NodeClass } from "./node";
-
+import {NodeClass} from './node';
 
 export class Bst {
-  public root?: NodeClass;
+  root?: NodeClass;
 
   constructor(root?: NodeClass) {
     this.root = root;
   }
 
-  public insert(val: number) {
+  private nullCheck(val: number) {
     if (this.root === undefined) {
-      return (this.root = new NodeClass(val));
+      this.root = new NodeClass(val);
+      return true;
     }
+    return false;
+  }
 
-    function bst(val: number, node?: NodeClass) {
+  // Run time 0(log n)
+  public insert(val: number) {
+    if (this.nullCheck(val)) return;
+
+    function dfs(val: number, node?: NodeClass) {
       if (node === undefined) return;
 
       // no duplicate insertions
       if (node.val === val) return;
 
       if (node.val > val && node.left === undefined) {
-        console.log('insertLeft');
         node.left = new NodeClass(val);
         return;
       }
 
       if (node.val < val && node.right === undefined) {
-        console.log('insertRight');
         node.right = new NodeClass(val);
         return;
       }
 
-      bst(val, node.left);
-      bst(val, node.right);
+      if (val < node.val) {
+        dfs(val, node.left);
+      } else {
+        dfs(val, node.right);
+      }
     }
 
-    bst(val, this.root);
+    dfs(val, this.root);
+  }
+
+  // Run time 0(n)
+  public balance() {
+    function bstToInOrderArray(node?: NodeClass): number[] {
+      if (node === undefined) return [];
+
+      const left = bstToInOrderArray(node.left);
+      const right = bstToInOrderArray(node.right);
+
+      return [...left, node.val, ...right];
+    }
+
+    function buildBalancedBst(arr: number[]) {
+      const len = arr.length;
+      if (len === 0) return undefined;
+
+      const middle = Math.floor(len / 2);
+      const root = new NodeClass(arr[middle]);
+
+      root.left = buildBalancedBst(arr.slice(0, middle));
+      root.right = buildBalancedBst(arr.slice(middle + 1));
+
+      return root;
+    }
+
+    const sortedArray = bstToInOrderArray(this.root);
+    this.root = buildBalancedBst(sortedArray);
+  }
+
+  // Run time 0(n)
+  public bstToArray() {
+    if (!this.root) return [];
+
+    const results = [];
+    let level: (NodeClass | undefined)[] = [this.root];
+
+    while (level.length) {
+      const newLevel = [];
+
+      for (const next of level) {
+        results.push(next?.val);
+
+        if (next && (next.left || next.right)) {
+          newLevel.push(next.left);
+          newLevel.push(next.right);
+        }
+      }
+
+      level = newLevel;
+    }
+
+    return results;
   }
 }
--- typescript-interview-questions/tests/class/bst.test.ts
+++ typescript-interview-questions/tests/class/bst.test.ts
@@ -2,11 +2,139 @@ import {Bst} from '../../src/class/bst';
 import {NodeClass} from '../../src/class/node';
 
 describe('bst class', () => {
-  test('insert', () => {
-    const tree = new Bst(new NodeClass(1));
-    tree.insert(2);
-    expect(tree.root).not.toBeUndefined();
-    expect(tree.root?.val).toBe(1);
-    expect(tree.root?.right?.val).toBe(2);
+  describe('insert method', () => {
+    test('insert into undefined root', () => {
+      const tree = new Bst();
+      tree.insert(199);
+
+      expect(tree.root?.val).toBe(199);
+    });
+
+    test('basic insert', () => {
+      const tree = new Bst(new NodeClass(1));
+      tree.insert(0);
+      tree.insert(2);
+      tree.insert(3);
+      tree.insert(-1);
+
+      expect(tree.root?.val).toBe(1);
+      expect(tree.root?.left?.val).toBe(0);
+      expect(tree.root?.left?.left?.val).toBe(-1);
+      expect(tree.root?.left?.right).toBeUndefined();
+      expect(tree.root?.right?.val).toBe(2);
+      expect(tree.root?.right?.left).toBeUndefined();
+      expect(tree.root?.right?.right?.val).toBe(3);
+      expect(tree.root?.right?.right?.left).toBeUndefined();
+      expect(tree.root?.right?.right?.right).toBeUndefined();
+    });
+
+    test('create unbalanced bst', () => {
+      const tree = new Bst(new NodeClass(100));
+      tree.insert(99);
+      tree.insert(98);
+      tree.insert(97);
+      tree.insert(96);
+
+      expect(tree.root?.val).toBe(100);
+      expect(tree.root?.left?.val).toBe(99);
+      expect(tree.root?.right).toBeUndefined();
+      expect(tree.root?.left?.left?.val).toBe(98);
+      expect(tree.root?.left?.right).toBeUndefined();
+      expect(tree.root?.left?.left?.left?.val).toBe(97);
+      expect(tree.root?.left?.left?.right).toBeUndefined();
+      expect(tree.root?.left?.left?.left?.left?.val).toBe(96);
+      expect(tree.root?.left?.left?.left?.right).toBeUndefined();
+      expect(tree.root?.left?.left?.left?.left?.left).toBeUndefined();
+      expect(tree.root?.left?.left?.left?.left?.right).toBeUndefined();
+    });
+
+    test("doesn't add duplicates", () => {
+      const tree = new Bst(new NodeClass(100));
+      tree.insert(99);
+      tree.insert(99);
+
+      expect(tree.root?.val).toBe(100);
+      expect(tree.root?.left?.val).toBe(99);
+      expect(tree.root?.right).toBeUndefined();
+      expect(tree.root?.left?.left).toBeUndefined();
+      expect(tree.root?.left?.right).toBeUndefined();
+    });
+  });
+
+  describe('balance method', () => {
+    test('will do nothing if root is undefined', () => {
+      const tree = new Bst();
+      tree.balance();
+
+      expect(tree.root).toBeUndefined();
+    });
+
+    test('will balance a left-skewed bst', () => {
+      const tree = new Bst(new NodeClass(100));
+      tree.insert(99);
+      tree.insert(98);
+      tree.insert(97);
+      tree.insert(96);
+      tree.balance();
+
+      expect(tree.root?.val).toBe(98);
+      expect(tree.root?.left?.val).toBe(97);
+      expect(tree.root?.right?.val).toBe(100);
+      expect(tree.root?.left?.left?.val).toBe(96);
+      expect(tree.root?.left?.right).toBeUndefined();
+      expect(tree.root?.left?.left?.left).toBeUndefined();
+      expect(tree.root?.left?.left?.right).toBeUndefined();
+      expect(tree.root?.right?.left?.val).toBe(99);
+      expect(tree.root?.right?.right).toBeUndefined();
+      expect(tree.root?.right?.left?.left).toBeUndefined();
+      expect(tree.root?.right?.left?.right).toBeUndefined();
+    });
+
+    test('will balance a right-skewed bst', () => {
+      const tree = new Bst(new NodeClass(0));
+      tree.insert(1);
+      tree.insert(2);
+      tree.insert(3);
+      tree.balance();
+
+      expect(tree.root?.val).toBe(2);
+      expect(tree.root?.left?.val).toBe(1);
+      expect(tree.root?.right?.val).toBe(3);
+      expect(tree.root?.left?.left?.val).toBe(0);
+      expect(tree.root?.left?.right).toBeUndefined();
+      expect(tree.root?.left?.left?.left).toBeUndefined();
+      expect(tree.root?.left?.left?.right).toBeUndefined();
+      expect(tree.root?.right?.left).toBeUndefined();
+      expect(tree.root?.right?.right).toBeUndefined();
+    });
+  });
+
+  describe('bstToArray method', () => {
+    test('will return the array representation of a normal bst', () => {
+      const tree = new Bst(new NodeClass(2));
+      tree.insert(1);
+      tree.insert(3);
+      tree.insert(4);
+      tree.insert(-1);
+      console.log('result', tree.bstToArray());
+      expect(tree.bstToArray()).toEqual([2, 1, 3, -1, undefined, undefined, 4]);
+    });
+
+    test('will return the array representation of a skewed bst', () => {
+      const tree = new Bst(new NodeClass(100));
+      tree.insert(50);
+      tree.insert(23);
+      tree.insert(-1);
+
+      expect(tree.bstToArray()).toEqual([
+        100,
+        50,
+        undefined,
+        23,
+        undefined,
+        -1,
+        undefined,
+      ]);
+    });
   });
 });
